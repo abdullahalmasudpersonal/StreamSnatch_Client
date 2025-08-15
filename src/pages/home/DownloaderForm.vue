322c393a27587294001b53238f8dc519307ffe34
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-08-16 00:38:17 +0600

    add production backend url

--- src/pages/home/DownloaderForm.vue
+++ src/pages/home/DownloaderForm.vue
@@ -45,7 +45,7 @@ const handleDownload = async () => {
   }
 
   try {
-    const response = await fetch('http://localhost:8000/api/download', {
+    const response = await fetch('https://streamsnatch-backend.onrender.com/api/download', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
